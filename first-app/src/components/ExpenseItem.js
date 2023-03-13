5172d3de537ba3dab7f6520eb9e69a24ce0de80a
Ahrar Hayat <ahrarhayat@gmail.com>
2023-03-13 14:37:05 +1100

    Expense Item Components added

---
+++ first-app/src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+  return (
+  <div className="expense-item">
+    <div>April 14 2023</div>
+    <div className="expense-item__description">
+      <h2>Car Insurance</h2>
+      <div className="expense-item__price">$250.30</div>
+    </div>
+  </div>);
+}
+
+export default ExpenseItem;
