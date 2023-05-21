e1aa7aca182044a3146479e7e47ac413c67653e8
Ahrar Hayat <ahrarhayat@gmail.com>
2023-05-21 16:48:06 +1000

    Added submit handler and two way binding

--- first-app/src/components/NewExpense/ExpenseForm.js
+++ first-app/src/components/NewExpense/ExpenseForm.js
@@ -12,6 +12,7 @@ const ExpenseForm = () => {
     // });
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value)
+        // console.log(enteredTitle)
         //setUserInput({...userInput,enteredTitle: event.target.value})
     }
 
@@ -24,19 +25,31 @@ const ExpenseForm = () => {
         setEnteredDate(event.target.value)
         // setUserInput({...userInput,enteredDate: event.target.value})
     }
-    return <form>
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+        console.log(expenseData)
+        setEnteredTitle('')
+        setEnteredAmount('')
+        setEnteredDate('')
+    };
+    return <form onSubmit={submitHandler}>
     <div className="new-expense__controls">
         <div className="new-expense__control">
             <label>Title</label>
-            <input type="text" onChange={titleChangeHandler}/>
+            <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
         </div>
         <div className="new-expense__control">
             <label>Amount</label>
-            <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+            <input type="number" value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler}/>
         </div>
         <div className="new-expense__control">
             <label>Date</label>
-            <input type="date" min="2019-01-01" max="2023-03-31" onChange={dateChangeHandler}/>
+            <input type="date" min="2019-01-01" value={enteredDate}  onChange={dateChangeHandler}/>
         </div>
     </div>
     <div className="new-expense__actions">
