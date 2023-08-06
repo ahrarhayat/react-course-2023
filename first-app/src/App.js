786fa4347834eaea54255b7a8e4bceabb9e3ff11
Ahrar Hayat <ahrarhayat@gmail.com>
2023-08-06 17:21:42 +1000

    New expense

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -12,7 +12,7 @@ const App = () => {
     { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: "e3",
-      title: "Car Insurance",
+      title: "AC Mirage",
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
