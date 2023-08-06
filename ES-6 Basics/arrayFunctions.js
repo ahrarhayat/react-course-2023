b8f34622f9a0769cf2312a12aa3359467aa466c7
Ahrar Hayat <ahrarhayat@gmail.com>
2023-08-06 16:45:41 +1000

    All irrelevant files removed

--- ES-6 Basics/arrayFunctions.js
+++
@@ -1,8 +0,0 @@
-const numbers = [1,2,3,4,5]
-
-//this function takes each of the elements in the array and does sth, here it is multiplying by 2
-const double = numbers.map((num) => {
-    return num * 2
-})
-
-console.log(double)
--- ES-6 Basics/importFile.js
+++
@@ -1,4 +0,0 @@
-import multiply2 from './exportFile.js'
-import { baseDate } from './exportFile.js'
-import { baseDate as newBase } from './exportFile.js'
-import { clean } from './exportFile.js'
