a2e68cc98e0e5802a67c386669b31949677eab66
Ahrar Hayat <ahrarhayat@gmail.com>
2023-08-28 12:23:39 +1000

    remove hard coded elements, add dynamic lists

--- first-app/src/components/Expenses/Expense.js
+++ first-app/src/components/Expenses/Expense.js
@@ -5,29 +5,23 @@ import ExpensesFilter from "./ExpensesFilter";
 import React, { useState } from "react";
 function Expense(props) {
   const [filteredYear, setFilteredYear] = useState("2023");
-  let filterInfoText = "2019 , 2020, 2021, 2022"
-  if(filteredYear === '2019')
-    {
-      filterInfoText = "2020 , 2021, 2022, 2023"
-    }
-    else if(filteredYear === '2020') {
-      filterInfoText = "2019 , 2021 , 2022 , 2023"
-    }
-    else if(filteredYear === '2021') {
-      filterInfoText = "2019 , 2020 , 2022 , 2023"
-    }
-    else if(filteredYear === '2022') {
-      filterInfoText = "2019 , 2020 , 2021 , 2023"
-    }
-    else if(filteredYear === '2023') {
-      filterInfoText = "2019 , 2020 , 2021 , 2022"
-    }
+  let filterInfoText = "2019 , 2020, 2021, 2022";
+  if (filteredYear === "2019") {
+    filterInfoText = "2020 , 2021, 2022, 2023";
+  } else if (filteredYear === "2020") {
+    filterInfoText = "2019 , 2021 , 2022 , 2023";
+  } else if (filteredYear === "2021") {
+    filterInfoText = "2019 , 2020 , 2022 , 2023";
+  } else if (filteredYear === "2022") {
+    filterInfoText = "2019 , 2020 , 2021 , 2023";
+  } else if (filteredYear === "2023") {
+    filterInfoText = "2019 , 2020 , 2021 , 2022";
+  }
   const changeYearHandler = (enteredYear) => {
     console.log("Expense.js");
     console.log(enteredYear);
     setFilteredYear(enteredYear);
     console.log("current year", filteredYear);
-    
   };
 
   return (
@@ -37,27 +31,14 @@ function Expense(props) {
           defaultYear={filteredYear}
           onChangeYear={changeYearHandler}
         />
-        <p className="para">Data for years {filterInfoText} is hidden</p>
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        />
+         <p className="para">Data for years {filterInfoText} is hidden</p>
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
